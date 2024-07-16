785fc81479ba67cfaf00fd3038dd16ed08f0f5bc
Gravizz <peeke0rr@gmail.com>
2024-07-16 08:39:03 +0700

    feat:add homepage ui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import HeaderBox from '@/components/HeaderBox';
+import TotalBalaceBox from '@/components/TotalBalaceBox';
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: 'Gravizz' };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn.firstName || 'Guest'}
+            subtext="Access and manage your account and transaction efficiently"
+          />
+          <TotalBalaceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
