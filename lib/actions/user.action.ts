062753aefab41dcad3bf913380d4267eafcfb40e
Gravizz <peeke0rr@gmail.com>
2024-07-26 22:14:17 +0700

    faet:add auth page ui

---
+++ lib/actions/user.action.ts
@@ -0,0 +1,19 @@
+'use server';
+
+import { count } from 'console';
+
+export const signIn = async () => {
+  try {
+    // Mutation / database / fetch
+  } catch (error) {
+    console.error('Error', error);
+  }
+};
+
+export const signUp = async (userData: SignUpParams) => {
+  try {
+    // create a user account
+  } catch (error) {
+    console.error('Error', error);
+  }
+};
