9380b6c36ff0c206396232e1024a478906b5d406
Gravizz <peeke0rr@gmail.com>
2024-08-01 04:29:16 +0700

    fix:fix something to deploy

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -14,16 +14,17 @@ const Sidebar = ({ user }: SiderbarProps) => {
   return (
     <section className="sidebar">
       <nav className="flex flex-col gap-4">
-        <Link href="/" className="mb-12 cursor-pointer items-center gap-2">
+        <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
           <Image
             src="/icons/logo.svg"
             width={34}
             height={34}
             alt="Horizon logo"
-            className=" size-[24px] max-xl:size-14"
+            className="size-[24px] max-xl:size-14"
           />
           <h1 className="sidebar-logo">Horizon</h1>
         </Link>
+
         {sidebarLinks.map((item) => {
           const isActive =
             pathname === item.route || pathname.startsWith(`${item.route}/`);
@@ -39,7 +40,9 @@ const Sidebar = ({ user }: SiderbarProps) => {
                   src={item.imgURL}
                   alt={item.label}
                   fill
-                  className={cn({ 'brightness-[3] invert-0': isActive })}
+                  className={cn({
+                    'brightness-[3] invert-0': isActive,
+                  })}
                 />
               </div>
               <p className={cn('sidebar-label', { '!text-white': isActive })}>
@@ -48,8 +51,10 @@ const Sidebar = ({ user }: SiderbarProps) => {
             </Link>
           );
         })}
+
         <PlaidLink user={user} />
       </nav>
+
       <Footer user={user} />
     </section>
   );
